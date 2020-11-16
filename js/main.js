86df95bb95be718cc1552b2e93cef1af4566366b
solclarke <69057860+solclarke@users.noreply.github.com>
2020-11-16 17:39:06 +0000

    Updated to jQuery

--- js/main.js
+++ js/main.js
@@ -1,52 +1,48 @@
 // Individual tab border and content:
 // Define variables
-const tabItems = document.querySelectorAll('.tab-item');
-const tabContentItems = document.querySelectorAll('.tab-content-item');
+const tabItems = $(".tab-item");
+const tabContentItems = $('.tab-content-item');
 
 // Select tab content item
 function selectItem(e) {
 	removeBorder();
 	removeShow();
 	// Add border to current tab
-	this.classList.add('tab-border');
+	$(this).addClass('tab-border');
 	// Grab content item from DOM
-	const tabContentItem = document.querySelector(`#${this.id}-content`);
+	const tabContentItem = $(`#${this.id}-content`);
 	// Add show class
-	tabContentItem.classList.add('show');
+	tabContentItem.addClass('show');
 }
 
 function removeBorder() {
-	tabItems.forEach(item => item.classList.remove('tab-border'));
+	tabItems.removeClass('tab-border');
 }
 
 function removeShow() {
-	tabContentItems.forEach(item => item.classList.remove('show'));
+	tabContentItems.removeClass('show');
 }
 
 // Listen for tab click
-tabItems.forEach(item => item.addEventListener('click', selectItem));
-
+tabItems.click(selectItem);
 
 
 // Homepage scroller icon:
-//Get the button
-mybutton = document.getElementById("chevronUp");
+// Get the button
+const mybutton = $('#chevronUp');
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
+	$(window).scroll(function() {
 	scrollFunction()
-};
+});
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+  if (document.documentElement.scrollTop > 20) 
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  {
+	mybutton.css("display", "block");
+  } else {
+	mybutton.css("display", "none");
+  }
 }
